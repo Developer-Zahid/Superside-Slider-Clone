402380a8feb20e2649881acfe79360cbe64ff748
Developer Zahid <coderexpertzahid@gmail.com>
2025-12-20 21:58:55 +0600

    Responsive done

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,33 +1,63 @@
-let slidersCommonOptions = {
-    loop: true,
-    speed: 700,
-    slidesPerView: 3,
-    // slidesPerView: "auto",
-    // loopAdditionalSlides: 1,
+const slideChangeSpeed = 700;
+const autoplayDelay = 3000;
+const largeScreenSize = window.matchMedia("(min-width: 1100px)");
+const totalSlides = document.querySelectorAll('[data-swiper-slider="thumbnail"] .swiper-slide').length || 0;
+let swiperThumbnail = null;
+let swiperPreview = null;
+const slidersCommonOptions = {
+	loop: true,
+    loopAdditionalSlides: totalSlides,
+	speed: slideChangeSpeed,
+	slidesPerView: 3,
+	breakpoints: {
+		450: {
+			slidesPerView: 5,
+		},
+		1100: {
+			slidesPerView: 3,
+		},
+	},
 };
 
-let swiperSliderThumbnail = new Swiper('[data-swiper-slider="thumbnail"]', {
-    ...slidersCommonOptions,
-    direction: "vertical",
-    // autoplay: {
-    //     delay: 2000,
-    //     disableOnInteraction: false,
-    // },
-    centeredSlides: true,
-    slideToClickedSlide: true,
-    watchSlidesProgress: true,
-    navigation: {
-        nextEl: '[data-swiper-navigation="thumbnail-next"]',
-        prevEl: '[data-swiper-navigation="thumbnail-prev"]',
-    },
-});
+function destroyExistingSwipers() {
+	if (swiperThumbnail instanceof Swiper) {
+		swiperThumbnail.destroy(true, true);
+		swiperThumbnail = null;
+	}
+	if (swiperPreview instanceof Swiper) {
+		swiperPreview.destroy(true, true);
+		swiperPreview = null;
+	}
+}
 
-let swiperSliderPreview = new Swiper('[data-swiper-slider="preview"]', {
-    ...slidersCommonOptions,
-    // grabCursor: true,
-    allowTouchMove: false,
-    virtualTranslate: true,
-});
+function initSwipers() {
+	destroyExistingSwipers();
 
-swiperSliderPreview.controller.control = swiperSliderThumbnail;
-swiperSliderThumbnail.controller.control = swiperSliderPreview;
+	swiperThumbnail = new Swiper('[data-swiper-slider="thumbnail"]', {
+		...slidersCommonOptions,
+        centeredSlides: true,
+        slideToClickedSlide: true,
+        watchSlidesProgress: true,
+        direction: largeScreenSize.matches ? "vertical" : "horizontal",
+        autoplay: {
+            delay: autoplayDelay,
+            disableOnInteraction: false,
+        },
+        navigation: {
+            nextEl: '[data-swiper-navigation="thumbnail-next"]',
+            prevEl: '[data-swiper-navigation="thumbnail-prev"]',
+        },
+	});
+
+	swiperPreview = new Swiper('[data-swiper-slider="preview"]', {
+		...slidersCommonOptions,
+        virtualTranslate: true,
+        allowTouchMove: false,
+	});
+
+	swiperPreview.controller.control = swiperThumbnail;
+	swiperThumbnail.controller.control = swiperPreview;
+}
+
+initSwipers();
+largeScreenSize.addEventListener("change", initSwipers);
